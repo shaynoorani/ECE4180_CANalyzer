ce19304de38350c1426a0b3f9b946024c7f93525
shaynoorani <113149316+shaynoorani@users.noreply.github.com>
2024-04-17 14:42:36 -0400

    Added HEX checking to make sure CAN ID is valid hex value

--- WindowsAppCanalyzer/WindowsAppCanalyzer/Form1.cs
+++ WindowsAppCanalyzer/WindowsAppCanalyzer/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO.Ports;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace WindowsAppCanalyzer
@@ -34,24 +35,33 @@ namespace WindowsAppCanalyzer
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Display a confirmation dialog
-            DialogResult result = MessageBox.Show("Are you sure you want to send this message?", "Warning! Sending incorrect messages can be fatal", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            string hexPattern = "^[0-9A-Fa-f]{3}$";
+            if (Regex.IsMatch(textBox2.Text,hexPattern)) // checks to make sure its a 3 digit hex 
+            {      
+                // Display a confirmation dialog
+                DialogResult result = MessageBox.Show("Are you sure you want to send this message?", "Warning! Sending incorrect messages can be fatal", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
 
-            // Check the user's choice
-            if (result == DialogResult.Yes)
-            {
-                // Send the message (you can add your logic here)
-                MessageBox.Show("Message sent successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                // Check the user's choice
+                if (result == DialogResult.Yes)
+                {
+                    // Send the message (you can add your logic here)
+                    MessageBox.Show("Message sent successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("I guess you didn't want to send it afterall", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
             }
-            else
+            else // Incorrect CAN ID
             {
-                MessageBox.Show("I guess you didn't want to send it afterall", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DialogResult result = MessageBox.Show("Make sure the CAN ID is the right length it should be 3 digits of HEX ex) FFF, 001 ect.", "CAN ID not correct", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
+   
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
